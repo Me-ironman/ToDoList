31abdf17f822199fceaa70a59c7fe8b0a06a0a63
moonly-Zh <1219980264@qq.com>
2020-06-29 22:43:11 +0800

    Optimize the adding process

--- src/Package/Package.java
+++ src/Package/Package.java
@@ -34,7 +34,7 @@ public class Package {
             System.out.println("运行出错");
         }
         while (true) {
-            System.out.println("请输入: 1(添加事项) 2(完成事项) 3(删除事项) 4(事项浏览) -1(保存退出)");
+            System.out.println("请输入:  1(添加事项) 2(完成事项) 3(删除事项) 4(事项浏览) -1(保存退出)");
             String order = new Scanner(System.in).next();
             if (order.equals("1")) {
                 add_item("");
@@ -47,7 +47,7 @@ public class Package {
                 delete_item();
             }
             if (order.equals("4")) {
-                System.out.println("请输入: 1(已完成) 2(未完成) 3(总览)");
+                System.out.println("请输入:  1(已完成) 2(未完成) 3(总览)");
                 Scanner input = new Scanner(System.in);
                 show_item(Integer.parseInt(input.next()) % 3);
             }
@@ -59,7 +59,7 @@ public class Package {
 
     public static void add_item(String work) {
         if (size == 100) {
-            System.out.println("事务已满,请删除后继续添加=_=");
+            System.out.println("事务已满,请删除后继续添加ovo");
         } else {
             if (work.equals("")) {
                 System.out.println("请输入事项描述,回车提交:");
@@ -85,7 +85,7 @@ public class Package {
     }
 
     public static void finish_item() {
-        System.out.println("请输完成事项id,回车提交:");
+        System.out.println("请输入完成事项id,回车提交:");
         Scanner input = new Scanner(System.in);
         int item_id = Integer.parseInt(input.next()) - 1;
         if (item_id >= size || item_id < 0) {
