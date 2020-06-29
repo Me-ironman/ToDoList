aba351c01332425b891abbef14f8ea69d714614f
moonly-Zh <1219980264@qq.com>
2020-06-30 00:01:09 +0800

    put the testfile in the first level directory

--- PackageTest.java
+++ PackageTest.java
@@ -1,69 +1,77 @@
-package source;
+package Package;
 
-import org.junit.After;
 import org.junit.Test;
 
 import java.io.ByteArrayInputStream;
 import java.io.InputStream;
-import java.lang.reflect.Field;
 
 import static org.junit.Assert.*;
 
 public class PackageTest {
-   
+
     @Test
     public void testadd_item() throws Exception {
+        System.out.println("add添加测试");
+        Package p=new Package();
         String input = "case1";
-        int actual = Package.getSize();
+        int actual = p.getSize();
+        int now = p.getSize();
         InputStream stdin = System.in;
         try{
             System.setIn(new ByteArrayInputStream(input.getBytes()));
-            Package.add_item(input);
+            p.add_item(input);
         }finally {
             System.setIn(stdin);
         }
-        assertEquals(1,actual+1);
+        assertEquals(1+now,actual+1);
 
     }
 
+
+
     @Test
     public void testdelete_item() {
+        System.out.println("delete删除测试");
+        Package p=new Package();
         String input1 = "case1";
         String input2 = "case2";
         String input3 = "1";
-        int actual = Package.getSize();
+        int actual = p.getSize();
+        int now=p.getSize();
         InputStream stdin = System.in;
         try{
             System.setIn(new ByteArrayInputStream(input1.getBytes()));
-            Package.add_item(input1);
+            p.add_item(input1);
             System.setIn(new ByteArrayInputStream(input2.getBytes()));
-            Package.add_item(input2);
-            actual = Package.getSize();
-            assertEquals(2,actual);
+            p.add_item(input2);
+            actual = p.getSize();
+            assertEquals(2+now,actual);
             System.setIn(new ByteArrayInputStream(input3.getBytes()));
-            Package.delete_item();
+            p.delete_item();
 
         }finally {
             System.setIn(stdin);
         }
-        actual = Package.getSize();
-        assertEquals(1,actual);
+        actual = p.getSize();
+        assertEquals(1+now,actual);
     }
 
     @Test
     public void testfinish_item() {
+        System.out.println("finish完成测试");
         String input1 = "case1";
         String input2 = "case2";
         String input3 = "2";
         int actual = Package.getSize();
         InputStream stdin = System.in;
+        int now = Package.getSize();
         try{
             System.setIn(new ByteArrayInputStream(input1.getBytes()));
             Package.add_item(input1);
             System.setIn(new ByteArrayInputStream(input2.getBytes()));
             Package.add_item(input2);
             actual = Package.getSize();
-            assertEquals(2,actual);
+            assertEquals(2+now,actual);
             System.setIn(new ByteArrayInputStream(input3.getBytes()));
             Package.finish_item();
         }finally {
@@ -74,29 +82,32 @@ public class PackageTest {
 
     @Test
     public void testshow_item() {//case1完成，其余未完成
+        Package p=new Package();
+        System.out.println("show展示测试");
         String input1 = "case1";
         String input2 = "case2";
         String input3 = "case3";
         String input4 = "1";
-        int actual = Package.getSize();
+        int actual = p.getSize();
+        int now = p.getSize();
         InputStream stdin = System.in;
         try{
             System.setIn(new ByteArrayInputStream(input1.getBytes()));
-            Package.add_item(input1);
+            p.add_item(input1);
             System.setIn(new ByteArrayInputStream(input2.getBytes()));
-            Package.add_item(input2);
+            p.add_item(input2);
             System.setIn(new ByteArrayInputStream(input3.getBytes()));
-            Package.add_item(input3);
-            actual = Package.getSize();
-            assertEquals(3,actual);
+            p.add_item(input3);
+            actual = p.getSize();
+            assertEquals(now+3,actual);
             System.setIn(new ByteArrayInputStream(input4.getBytes()));
-            Package.finish_item();
+            p.finish_item();
             System.out.println("---------全部打印----------");
-            Package.show_item(0);
+            p.show_item(0);
             System.out.println("---------已完成的----------");
-            Package.show_item(1);
+            p.show_item(1);
             System.out.println("---------未完成的----------");
-            Package.show_item(2);
+            p.show_item(2);
         }finally {
             System.setIn(stdin);
         }
